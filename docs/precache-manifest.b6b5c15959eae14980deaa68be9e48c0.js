58f8b852759b328aa9f2d359007b78878be1535a
Joshua Cid <joshua.cid@virginiogomez.cl>
2020-07-12 23:28:38 -0400

    docs agregado

---
+++ docs/precache-manifest.b6b5c15959eae14980deaa68be9e48c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb07d8839ac74cab11d5cc2de28858bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bc5f00194f540b73a75",
+    "url": "/static/css/main.dbe28895.chunk.css"
+  },
+  {
+    "revision": "027ae5944804431f3db5",
+    "url": "/static/js/2.9f575afa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f575afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bc5f00194f540b73a75",
+    "url": "/static/js/main.8094bd7d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
